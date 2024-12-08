582e60ced271d20f0170420ddcba5058c76452bd
Eone-tgx <eone1024@gmail.com>
2024-12-08 16:32:22 +0200

    task7-8

--- task8.js
+++ task8.js
@@ -2,7 +2,17 @@
 // рядок str і символ char.Функція повинна повертати новий рядок,
 //   у якому всі входження символу char видалені. Використовуйте цикл while
 
+function removeCharacter(str, char) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] !== char) {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
+
 // Приклади використання:
-console.log(removeCharacter('Hello, world!', 'o')) // "Hell, wrld!"
-console.log(removeCharacter('JavaScript', 'a')) // "JvScript"
-console.log(removeCharacter('12345', '3')) // "1245"
+console.log(removeCharacter("Hello, world!", "o")); // "Hell, wrld!"
+console.log(removeCharacter("JavaScript", "a")); // "JvScript"
+console.log(removeCharacter("12345", "3")); // "1245"
