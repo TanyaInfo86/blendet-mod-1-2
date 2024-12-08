a1a1bb1478e1810f1f26c2680d30c9a7ecdcaa70
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2024-12-08 14:00:34 +0100

    Update

--- task2.js
+++ task2.js
@@ -2,8 +2,10 @@
 // індекс початку start та індекс кінця end.
 // Функція повинна повертати підрядок, що починається з індексу start
 // і закінчується індексом end(не включаючи його).
-
+function getSubstring(str, start, end) {
+  return str.slice(start, end);
+}
 // Приклади використання:
-console.log(getSubstring('Hello, world!', 7, 12)) // "world"
-console.log(getSubstring('JavaScript', 4, 10)) // "Script"
-console.log(getSubstring('Slice method', 0, 5)) // "Slice"
+console.log(getSubstring("Hello, world!", 7, 12)); // "world"
+console.log(getSubstring("JavaScript", 4, 10)); // "Script"
+console.log(getSubstring("Slice method", 0, 5)); // "Slice"
