5653f1a0412518742b3048f9f8f2d190bbf02268
maria <fesychmarimia@gmail.com>
2024-12-08 14:56:32 +0100

    fifth task

--- task5.js
+++ task5.js
@@ -4,6 +4,17 @@
 //   функція повинна повертати порожній рядок.
 
 // Приклади використання:
-console.log(extractBetween('Hello, [world]!', '[', ']')) // "world"
-console.log(extractBetween('Hello, [world]!', '{', '}')) // ""
-console.log(extractBetween('Hello, {world}!', '{', '}')) // "world"
+function extractBetween(str, start, end) {
+  const indexStart = str.indexOf(start);
+  if (indexStart === -1) {
+    return "";
+  }
+  const indexEnd = str.indexOf(end);
+  if (indexEnd === -1) {
+    return "";
+  }
+  return str.slice(indexStart + start.length, indexEnd);
+}
+console.log(extractBetween("Hello, [world]!", "[", "]")); // "world"
+console.log(extractBetween("Hello, [world]!", "{", "}")); // ""
+console.log(extractBetween("Hello, {world}!", "{", "}")); // "world"
